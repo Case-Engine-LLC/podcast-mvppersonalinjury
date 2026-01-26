4aef81e97efdde6414c5867083e82d0574853477
C Gallic <cgallic@gmail.com>
2026-01-26 12:14:42 -0400

    fix(hero): add missing ArrowRight import

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import Link from 'next/link'
-import { PlayCircle } from 'lucide-react'
+import { PlayCircle, ArrowRight } from 'lucide-react'
 
 const Hero = () => {
   return (
